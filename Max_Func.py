f1af22cc9409871517ee4d5a678bf05a176cd5e5
Yefri Stiven Barrero Solano <yefbarrero2001@gmail.com>
2025-10-16 21:10:58 -0500

    Código maximizando función

--- Max_Func.py
+++ Max_Func.py
@@ -5,26 +5,36 @@ import matplotlib.pyplot as plt
 def f(x):
     return x**2 - 3*x + 4
 
-# Coeficientes de la función cuadrática: f(x) = ax^2 + bx + c
-a = 1
-b = -3
+# Definir el intervalo cerrado [x_min, x_max]
+x_min = 0  # Límite inferior del intervalo
+x_max = 3  # Límite superior del intervalo
 
-# Calcular el vértice
-x_vertex = -b / (2 * a)
-y_vertex = f(x_vertex)
+# Evaluar la función en los extremos del intervalo
+f_min = f(x_min)
+f_max = f(x_max)
 
-print(f"El vértice de la función está en x = {x_vertex}, f(x) = {y_vertex}")
+# Encontrar el máximo en el intervalo
+if f_min > f_max:
+    x_maximo = x_min
+    f_maximo = f_min
+else:
+    x_maximo = x_max
+    f_maximo = f_max
+
+print(f"El máximo en el intervalo [{x_min}, {x_max}] está en x = {x_maximo}, f(x) = {f_maximo}")
 
 # Generar puntos para graficar
-x = np.linspace(-1, 4, 100)
+x = np.linspace(x_min - 0.5, x_max + 0.5, 100)
 y = f(x)
 
-# Graficar
+# Graficar la función y el máximo
 plt.plot(x, y, label='f(x) = x^2 - 3x + 4')
-plt.scatter([1.5], [1.75], color='red', label='Vértice (1.5, 1.75)')
+plt.scatter([x_maximo], [f_maximo], color='red', label=f'Máximo en x={x_maximo}')
+plt.axvline(x_min, color='gray', linestyle='--', label='Límites del intervalo')
+plt.axvline(x_max, color='gray', linestyle='--')
 plt.xlabel('x')
 plt.ylabel('f(x)')
-plt.title('Gráfica de la función')
+plt.title('Máximo de la función en el intervalo cerrado')
 plt.legend()
 plt.grid(True)
 plt.show()
